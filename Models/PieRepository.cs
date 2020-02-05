e7b9f25c1ab761101ff0c4dbecc08ebd8afa0d82
Todd Bartelt <tbartelt@e-hfg.com>
2020-02-05 13:20:11 -0600

    Add project files.

---
+++ Models/PieRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class PieRepository: IPieRepository
+    {
+        public readonly AppDbContext _appDbContext;
+
+        public PieRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Pie> AllPies 
+        {
+            get
+            {
+                return _appDbContext.Pies.Include(c => c.Category);
+            }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek => throw new NotImplementedException();
+
+        public Pie GetPieById(int pieId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
