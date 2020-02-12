e53a49958d445a949acb8491e71b1151837269ca
Todd Bartelt <tbartelt@e-hfg.com>
2020-02-12 17:20:04 -0600

    Completed module 8. Migrated database for IdentityDbContext.

---
+++ Migrations/20200212192532_OrderTotal.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BethanysPieShop.Migrations
+{
+    public partial class OrderTotal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderDetail",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "OrderTotal",
+                table: "Orders",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderTotal",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrderDetail",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ Models/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public interface IOrderRepository
+    {
+        public interface IOrderRepository
+        {
+            void CreateOrder(Order order);
+        }
+
+        void CreateOrder(Order order);
+    }
+}
